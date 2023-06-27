333d6f593f8ab3dae3930b85699d190c434cc237
VazgenBablumyan <vazgeb@gmail.com>
2023-06-27 21:12:51 +0400

    feat section.jsx, report.jsx, add button report and logic

--- src/Components/Section/Section.jsx
+++ src/Components/Section/Section.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Card'
 import './section.css'
+import Report from './Report'
 
 export default function ({cards}) {
+  const [showReport, setShowReport] = useState(false)
+    const handleClick =() => setShowReport(!showReport)
   return (
     <div className='section'>
         <Card cards = {cards} />
+        <button className='report'onClick={handleClick}>Report</button>
+        {!showReport ? <Report />: null}
     </div>
   )
 }
